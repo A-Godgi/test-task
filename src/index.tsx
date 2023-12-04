c817153881bda40732f6cfcc6958daa3001b4c41
A-Godgi <agodgi@gmail.com>
2023-12-04 15:09:12 +0200

    connect redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+//Normalize.css
+import 'normalize.css';
+
+//Main component
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//Redux store
+import { Provider } from 'react-redux';
+import {configureStore} from "@reduxjs/toolkit";
+import {testTaskApi} from "./store/api";
+
+const store = configureStore({ reducer: {
+        [testTaskApi.reducerPath]: testTaskApi.reducer,
+    }, devTools: true, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(testTaskApi.middleware)});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
